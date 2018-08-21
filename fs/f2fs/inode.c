01cc1fd651ffabecc8f2719209e2dd94df2d3b2c
Daniel Rosenberg <drosen@google.com>
2018-08-20 19:21:43 -0700

    f2fs: checkpoint disabling
    Note that, it requires "f2fs: return correct errno in f2fs_gc".
    
    This adds a lightweight non-persistent snapshotting scheme to f2fs.
    
    To use, mount with the option checkpoint=disable, and to return to
    normal operation, remount with checkpoint=enable. If the filesystem
    is shut down before remounting with checkpoint=enable, it will revert
    back to its apparent state when it was first mounted with
    checkpoint=disable. This is useful for situations where you wish to be
    able to roll back the state of the disk in case of some critical
    failure.
    
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    [Jaegeuk Kim: use SB_RDONLY instead of MS_RDONLY]
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    (cherry picked from commit 7ea0d208d6597df11f7ad965ffd2632c18aa461e)
    (cherry picked from commit c12b54ad5500cef094ef775fb9c21a06325e7757)

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -607,6 +607,9 @@ int f2fs_write_inode(struct inode *inode, struct writeback_control *wbc)
 	if (!is_inode_flag_set(inode, FI_DIRTY_INODE))
 		return 0;
 
+	if (f2fs_is_checkpoint_ready(sbi))
+		return -ENOSPC;
+
 	/*
 	 * We need to balance fs here to prevent from producing dirty node pages
 	 * during the urgent cleaning time when runing out of free sections.
@@ -688,14 +691,11 @@ no_delete:
 	stat_dec_inline_dir(inode);
 	stat_dec_inline_inode(inode);
 
-	if (unlikely(is_inode_flag_set(inode, FI_DIRTY_INODE))) {
+	if (likely(!is_set_ckpt_flags(sbi, CP_ERROR_FLAG) &&
+				!is_sbi_flag_set(sbi, SBI_CP_DISABLED)))
+		f2fs_bug_on(sbi, is_inode_flag_set(inode, FI_DIRTY_INODE));
+	else
 		f2fs_inode_synced(inode);
-		f2fs_msg(sbi->sb, KERN_WARNING,
-			 "inconsistent dirty inode:%u entry found during eviction\n",
-			 inode->i_ino);
-		if (!is_set_ckpt_flags(sbi, CP_ERROR_FLAG))
-			f2fs_bug_on(sbi, 1);
-	}
 
 	/* ino == 0, if f2fs_new_inode() was failed t*/
 	if (inode->i_ino)
