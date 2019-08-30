013ca876e7fcee332b2fd1a860b5ea2ed886377f
freak07 <michalechner92@googlemail.com>
2019-08-30 17:10:57 +0200

    build: use lld
    (cherry picked from commit 777ffbe6f9e1b138dbdcb98a362d446a32b46501)

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -18,11 +18,12 @@ export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r365631/lib64:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
 echo 
-make CC=clang O=out kirisakura_defconfig
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out kirisakura_defconfig
 
 echo
 echo "Build The Good Stuff"
