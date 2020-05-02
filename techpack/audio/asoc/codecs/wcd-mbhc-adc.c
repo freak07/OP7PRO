fe3fa3327b741d2523af4effdda22c8211c84f94
freak07 <michalechner92@googlemail.com>
2020-05-02 15:04:44 +0200

    techpack: audio: fix a few clang 11 errors

--- techpack/audio/asoc/codecs/wcd-mbhc-adc.c
+++ techpack/audio/asoc/codecs/wcd-mbhc-adc.c
@@ -903,13 +903,15 @@ enable_supply:
 	if (mbhc->mbhc_cb->mbhc_micbias_control)
 		wcd_mbhc_adc_update_fsm_source(mbhc, plug_type);
 exit:
-    if (plug_type == MBHC_PLUG_TYPE_HEADSET)
+    if (plug_type == MBHC_PLUG_TYPE_HEADSET) {
         mbhc->micbias_enable = true;
+    }
 
 	if (mbhc->mbhc_cb->mbhc_micbias_control &&
-	    !mbhc->micbias_enable)
+	    !mbhc->micbias_enable) {
 		mbhc->mbhc_cb->mbhc_micbias_control(codec, MIC_BIAS_2,
 						    MICB_DISABLE);
+	}
 
 	/*
 	 * If plug type is corrected from special headset to headphone,
